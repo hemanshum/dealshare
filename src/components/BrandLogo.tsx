0c2d2002b8a65de038ede37b0c3343e303288429
Hemanshu MMahajan <hemanshu.mahajan@gmail.com>
2024-10-31 21:59:19 +0530

    next and clerk setup and homepage partially completed

---
+++ src/components/BrandLogo.tsx
@@ -0,0 +1,8 @@
+import { Globe2Icon } from 'lucide-react'
+
+export function BrandLogo() {
+  return <div className='flex items-center gap-2 font-semibold flex-shrink-0  text-lg'>
+    <Globe2Icon className='size-8' />
+    <span>Easy PPP</span>
+  </div>
+}
