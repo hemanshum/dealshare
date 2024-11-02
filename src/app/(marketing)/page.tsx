5c8513d9cb741613007594bedcb332ed3c3f9cc5
Hemanshu MMahajan <hemanshu.mahajan@gmail.com>
2024-11-02 18:03:15 +0530

    home page completed

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,16 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { SignUpButton } from "@clerk/nextjs";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, CheckIcon } from "lucide-react";
 import Link from "next/link";
 import { NeonIcon } from "./_icons/Neon";
 import { ClerkIcon } from "./_icons/Clerk";
 import { subscriptionTiersInOrder } from "@/data/subscriptionTiers";
 import {
   Card,
+  CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { formatCompactNumber } from "@/lib/formatters";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
+import { BrandLogo } from "@/components/BrandLogo";
 
 export default function HomePage() {
   return (
@@ -78,6 +84,75 @@ export default function HomePage() {
           ))}
         </div>
       </section>
+      <footer className='container pt-16 pb-8 flex flex-col sm:flex-row gap-8 sm:gap-4 justify-between items-start'>
+        <Link href='/'>
+          <BrandLogo />
+        </Link>
+        <div className='flex flex-col sm:flex-row gap-8'>
+          <div className='flex flex-col gap-8'>
+            <FooterLinkGroup
+              title='Help'
+              links={[
+                { label: "PPP Discounts", href: "#" },
+                { label: "Discount API", href: "#" },
+              ]}
+            />
+            <FooterLinkGroup
+              title='Solutions'
+              links={[
+                { label: "Newsletter", href: "#" },
+                { label: "SaaS Business", href: "#" },
+                { label: "Online Courses", href: "#" },
+              ]}
+            />
+          </div>
+          <div className='flex flex-col gap-8'>
+            <FooterLinkGroup
+              title='Features'
+              links={[{ label: "PPP Discounts", href: "#" }]}
+            />
+            <FooterLinkGroup
+              title='Tools'
+              links={[
+                { label: "Salary Converter", href: "#" },
+                { label: "Coupon Generator", href: "#" },
+                { label: "Stripe App", href: "#" },
+              ]}
+            />
+            <FooterLinkGroup
+              title='Company'
+              links={[
+                { label: "Affiliate", href: "#" },
+                { label: "Twitter", href: "#" },
+                { label: "Terms of Service", href: "#" },
+              ]}
+            />
+          </div>
+          <div className='flex flex-col gap-8'>
+            <FooterLinkGroup
+              title='Integrations'
+              links={[
+                { label: "Lemon Squeezy", href: "#" },
+                { label: "Gumroad", href: "#" },
+                { label: "Stripe", href: "#" },
+                { label: "Chargebee", href: "#" },
+                { label: "Paddle", href: "#" },
+              ]}
+            />
+            <FooterLinkGroup
+              title='Tutorials'
+              links={[
+                { label: "Any Website", href: "#" },
+                { label: "Lemon Squeezy", href: "#" },
+                { label: "Gumroad", href: "#" },
+                { label: "Stripe", href: "#" },
+                { label: "Chargebee", href: "#" },
+                { label: "Paddle", href: "#" },
+              ]}
+            />
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
@@ -91,14 +166,75 @@ function PricingCard({
   canAccessAnalytics,
   canCustomizeBanner,
 }: (typeof subscriptionTiersInOrder)[number]) {
+  const isMostPopular = name === "Standard";
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>${priceInCents / 100} /mo</CardTitle>
+        <div className='text-accent font-semibold mb-8'>{name}</div>
+        <CardTitle className='text-xl font-bold'>
+          ${priceInCents / 100} /mo
+        </CardTitle>
         <CardDescription>
-          {maxNumberOfVisits} pricing page visits/mo
+          {formatCompactNumber(maxNumberOfVisits)} pricing page visits/mo
         </CardDescription>
       </CardHeader>
+      <CardContent>
+        <SignUpButton>
+          <Button
+            className='text-lg w-full rounded-lg'
+            variant={isMostPopular ? "accent" : "default"}
+          >
+            Get Started
+          </Button>
+        </SignUpButton>
+      </CardContent>
+      <CardFooter className='flex flex-col gap-4 items-start'>
+        <Feature className='font-bold'>
+          {maxNumberOfProducts}{" "}
+          {maxNumberOfProducts === 1 ? "product" : "products"}
+        </Feature>
+        <Feature>DealShare Discounts</Feature>
+        {canAccessAnalytics && <Feature>Advance analytics</Feature>}
+        {canCustomizeBanner && <Feature>Banner customization</Feature>}
+        {canRemoveBranding && <Feature>Remove Easy PPP branding</Feature>}
+      </CardFooter>
     </Card>
+  );
+}
+
+function Feature({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <CheckIcon className='size-4 stroke-accent bg-accent/25 rounded-full p-0.5' />
+      <span>{children}</span>
+    </div>
+  );
+}
+
+function FooterLinkGroup({
+  title,
+  links,
+}: {
+  title: string;
+  links: { label: string; href: string }[];
+}) {
+  return (
+    <div className='flex flex-col gap-4'>
+      <h3 className='font-semibold'>{title}</h3>
+      <ul className='flex flex-col gap-2 text-sm'>
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
